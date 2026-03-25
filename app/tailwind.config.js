8b8ceaf664f9dc1f04769a0d9db934ef0fc6b1a2
thisisrick25 <swapnaneel06@gmail.com>
2026-03-25 23:02:52 +0530

    feat(app): add Tailwind CSS config

---
+++ app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{svelte,js,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        // Custom colors can be added here
+      },
+    },
+  },
+  plugins: [],
+};
